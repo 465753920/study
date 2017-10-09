0b5cf27302a25f4352e8c9ff1df24b1962a9eac4
ShenQiuS <465753920@qq.com>
2017-10-09 18:10:24 +0800

    增加JPA-hibernate样例

---
+++ jpa-hibernate/src/main/java/org/xiaomao/jpa/manager/EmployeeManager.java
@@ -0,0 +1,38 @@
+package org.xiaomao.jpa.manager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.xiaomao.jpa.entity.Employee;
+
+public class EmployeeManager {
+
+	public static void main(String[] args) {
+
+		EmployeeManager em = new EmployeeManager();
+
+		em.createEmployee();
+
+	}
+
+	public void createEmployee() {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_Demo_1");
+
+		EntityManager entitymanager = emfactory.createEntityManager();
+		entitymanager.getTransaction().begin();
+
+		Employee employee = new Employee();
+		employee.setEid(1202);
+		employee.setEname("ShenQiuS");
+		employee.setSalary(60000);
+		employee.setDeg("Software Engineer");
+
+		entitymanager.persist(employee);
+		entitymanager.getTransaction().commit();
+
+		entitymanager.close();
+		emfactory.close();
+	}
+
+}

76da087b7a05019b42e7cba5ecdc695e3f496696
ShenQiuS <465753920@qq.com>
2017-10-16 18:06:54 +0800

    增加logback日志输出

--- hibernate/src/main/java/org/xiaomao/hibernate/managers/ShopManager.java
+++ hibernate/src/main/java/org/xiaomao/hibernate/managers/ShopManager.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xiaomao.hibernate.entity.Shop;
 import org.xiaomao.hibernate.utils.HibernateUtil;
 
 public class ShopManager {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Test
 	public void add() {
@@ -20,6 +24,8 @@ public class ShopManager {
 		session.save(shop);
 
 		session.getTransaction().commit();
+		
+		logger.debug("添加成功");
 	}
 
 	@Test
@@ -30,7 +36,7 @@ public class ShopManager {
 		List<Shop> list = session.createQuery(" FROM Shop ").list();
 
 		for (Shop s : list) {
-			System.out.println("名称：" + s.getName() + "，地址：" + s.getAddress());
+			logger.info("名称：" + s.getName() + "，地址：" + s.getAddress());
 		}
 
 		session.getTransaction().commit();

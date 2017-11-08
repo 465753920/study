ab943783b0706cbdf073b8ca36b49d5feedc6047
ShenQiuS <465753920@qq.com>
2017-11-08 18:06:37 +0800

    aaa

--- servlet/servlet/src/main/java/org/xiaomao/servlet/HelloWorld.java
+++ servlet/servlet/src/main/java/org/xiaomao/servlet/HelloWorld.java
@@ -1,8 +1,6 @@
 package org.xiaomao.servlet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
--- servlet/servlet/src/test/java/org/xiaomao/httpclient/SimpleDemo.java
+++ servlet/servlet/src/test/java/org/xiaomao/httpclient/SimpleDemo.java
@@ -1,22 +1,32 @@
 package org.xiaomao.httpclient;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.junit.Test;
 
 public class SimpleDemo {
 
-	public void testPost() {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet("url");
+	@Test
+	public void testGet() {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("http://localhost:8080/servlet/helloservlet");
+		CloseableHttpResponse response = null;
+
 		try {
-			CloseableHttpResponse response = httpclient.execute(httpGet);
+			response = httpClient.execute(httpGet);
 			System.out.println(response.getStatusLine());
 			HttpEntity entity = response.getEntity();
 			EntityUtils.consume(entity);
@@ -25,7 +35,40 @@ public class SimpleDemo {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
+			// 关闭流
+			try {
+				if (response != null)
+					response.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+
+	public void testPost() {
+
+		try {
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			HttpPost httpPost = new HttpPost("url");
+
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			nvps.add(new BasicNameValuePair("username", "vip"));
+			nvps.add(new BasicNameValuePair("password", "secret"));
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+
+			CloseableHttpResponse response = httpClient.execute(httpPost);
+
+			try {
+				System.out.println(response.getStatusLine());
+				HttpEntity httpEntity = response.getEntity();
+				EntityUtils.consume(httpEntity);
+			} finally {
+				response.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
